3b93a89fad0b9f60fdd43b530dcd1511e69ec395
Sam Bradshaw <bradshaw.sa@yahoo.com>
2025-08-27 22:31:49 +0100

    Boiler plate for http server

---
+++ json.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"encoding/json"
+	"net/http"
+)
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	if code > 499 {
+		log.Println("Responding with 5XX error: ", msg)
+	}
+
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+
+	respondWithJson(w, code, errorResponse{
+		Error: msg,
+	})
+}
+
+func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Build to marshal json response: %v", payload)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.Header().Add("Content-Type:", "application/json")
+	w.WriteHeader(code)
+	w.Write(data)
+}
--- main.go
+++ main.go
@@ -2,8 +2,52 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"log"
+	"net/http"
+	"github.com/joho/godotenv"
+	"github.com/go-chi/cors"
+	"github.com/go-chi/chi"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	err := godotenv.Load(".env")
+	if err != nil {	
+		log.Fatal("Error loading .env file")
+	}
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+
+	v1Router.Get("/ready", handlerReadiness)
+	v1Router.Post("/ready2", handlerReadiness)
+	v1Router.Get("/error", handleError)
+
+	router.Mount("/v1", v1Router)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    fmt.Sprintf(":%s", portString),
+	}
+
+	log.Println("Starting server on port", portString)
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
